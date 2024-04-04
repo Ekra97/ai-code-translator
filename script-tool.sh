8d19d8377244f11c78fb0baf4e2d4856c414f2fd
Ekra97 <124062167+Ekra97@users.noreply.github.com>
2024-04-04 16:21:42 -0700

    Create script-tool.sh

---
+++ script-tool.sh
@@ -0,0 +1,18 @@
+- git clone https://github.com/Ekra97/ai-code-translator.git
+- cd ai-code-translator/
+- npm i 
+- npx update-browserslist-db@latest
+- npm run-script dev
+- npm run-script build
+- npm run-script start
+- npm run-script lint 
+- npm config get [key ...]
+- npm get [key ...]
+
+npm help config
+npm config set <key>=<value> [<key>=<value> ...]
+npm config get [<key> [<key> ...]]
+npm config delete <key> [<key> ...]
+npm config list [--json]
+npm config edit
+npm config fix
